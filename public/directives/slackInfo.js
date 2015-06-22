11229bbe418e4175de1123160a38492c59f7b52f
alibojar <ali@13strides.com>
2015-06-22 12:36:13 +0100

    working

--- public/directives/slackInfo.js
+++ public/directives/slackInfo.js
@@ -1,16 +1,38 @@
 ;
 (function() {
-  var slackInfoController = function(slackServer) {
+  var slackInfoController = function(slackServer, $scope) {
     var slackInfoVm = this;
 
+    slackInfoVm.response = '';
+    slackInfoVm.deleteCount = 0;
     slackInfoVm.token = 'xoxp-2191906948-2195788498-6458352101-022c13';
-    slackInfoVm.daysList = [1, 2, 7, 14, 30, 60, 90];
+    slackInfoVm.daysList = ['now', 1, 2, 7, 14, 30, 60, 90];
+    slackInfoVm.days = slackInfoVm.daysList[slackInfoVm.daysList.length - 1];
 
-    slackInfoVm.deleteFiles = function(){
-    	slackServer.getFiles(slackInfoVm.token, slackInfoVm.days)
-    		.then(function(data){
-    			console.log(data);
-    		});
+    slackServer.getUsers(slackInfoVm.token)
+      .then(function(data) {
+        var users = data.data.members;
+        slackInfoVm.usersList = users;
+      });
+
+    slackInfoVm.deleteFiles = function() {
+      if (!slackInfoVm.token || !slackInfoVm.user ||! slackInfoVm.days){
+        alert('All options must be selected');
+        return;
+      }
+      slackServer.getFiles(slackInfoVm.token, slackInfoVm.user, slackInfoVm.days)
+        .then(function(data) {
+          var response = data.data;
+          if (!response.ok) return;
+          if (response.files.length === 0) alert('No file left');
+          response.files.forEach(function(file) {
+            slackInfoVm.response += 'Deleting: ' + file.title;
+            slackInfoVm.response += '\n\r';
+            slackServer.deleteFiles(slackInfoVm.token, file.id).then(function(data) {
+              slackInfoVm.deleteCount++;
+            });
+          });
+        });
     }
 
   }
@@ -27,6 +49,6 @@
   angular
     .module('slackApp')
     .directive('slackInfo', slackInfo)
-  slackInfo.$inject = ['slackServer'];
+  slackInfoController.$inject = ['slackServer','$scope'];
 
 })();
--- public/factories/slackServer.js
+++ public/factories/slackServer.js
@@ -3,27 +3,51 @@
   var slackServer = function($http) {
     var url = 'https://slack.com/api/';
 
-    var getFiles = function(token, days) {
+    var getUsers = function(token) {
+      var _url = url + 'users.list';
+      var postData = {
+        token: token
+      };
+      return $http({
+        url: _url,
+        method: 'GET',
+        params: postData
+      })
+
+    }
+
+    var getFiles = function(token, user, days) {
       var _url = url + 'files.list';
       var d = new Date();
       var daysAgo = d.setDate(d.getDate() - days);
-      var user = 'U025RP6EN';
-
-
       var postData = {
-        ts_to: daysAgo / 1000,
+        ts_to: Math.round(daysAgo / 1000),
         user: user,
         token: token
       };
-
-      return $http.post(_url, postData);
+      return $http({
+        url: _url,
+        method: 'GET',
+        params: postData
+      });
     };
 
-    var deleteFiles = function() {
+    var deleteFiles = function(token, fileId) {
+      var _url = url + 'files.delete';
+      var postData = {
+        token: token,
+        file: fileId
+      };
 
+      return $http({
+        url: _url,
+        method: 'GET',
+        params: postData
+      });
     };
 
     return {
+      getUsers: getUsers,
       getFiles: getFiles,
       deleteFiles: deleteFiles
     }
